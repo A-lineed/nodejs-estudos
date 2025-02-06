90b5bb0f6977455afb59ed329475556b03290cc5
A-lineed <aline.edvania.franca@outlook.com>
2025-02-06 19:10:02 -0300

    Salvando usuários em memória

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello amor')
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Aline Edvania',
+            email: 'aline@gmail.com',
+        })  
+
+        return res.end('Usuário criado')
+    }
+    
+    return res.end('Hello Wolrd')
 
 })
 
